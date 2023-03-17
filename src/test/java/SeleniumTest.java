cf0e5bfa2041c5ddde43bc48e05801ee63ed66b6
Tomek Zubrzycki <zpt77@wp.pl>
2023-03-17 20:01:14 +0100

    ChromeDriver created

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,9 +1,16 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import com.github.javafaker.Faker;
+
+import java.time.Duration;
 
 public class SeleniumTest {
 
@@ -17,10 +24,26 @@ public class SeleniumTest {
     }
 
     @Test
-    public void testMethod() {
+    public void openGoogle() {
         driver.get("https://www.google.com/");
+        WebElement element = driver.findElement(By.cssSelector("#L2AGLb > div"));
+        element.click();
     }
 
+    @Test
+    public void openParabank() {
+        driver.get("https://parabank.parasoft.com/");
+    }
+
+    @Test
+    public void register() {
+        WebElement element = driver.findElement(By.cssSelector("#loginPanel > p:nth-child(3) > a"));
+        element.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement firstName = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#customer\\.firstName")));
+
+    }
 
 }
 
